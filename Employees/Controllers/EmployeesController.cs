cd46f81706f997fe5b658c96ae97b6bc9196ee71
daniel.seara <daniel.seara@ITEDESKTOP07>
2017-06-01 11:43:08 +0100

    Add project files.

---
+++ Employees/Controllers/EmployeesController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Employees.Models;
+
+namespace Employees.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private EmployeesEntities db = new EmployeesEntities();
+
+        // GET: Employees
+        public ActionResult Index()
+        {
+            return View(db.Employees.ToList());
+        }
+
+        // GET: Employees/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Surname,Email,CreationDate")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                employee.CreationDate = DateTime.Now;
+                db.Employees.Add(employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(employee);
+        }
+
+        // GET: Employees/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Positions = new SelectList(db.Positions, "Id", "Name");
+            return View(employee);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public bool Edit([Bind(Include = "Id,Name,Surname,Email,CreationDate,EmployeesPositions")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                foreach (var employeePosition in employee.EmployeesPositions)
+                {
+                    if (employeePosition.CreationDate == DateTime.MinValue)
+                    {
+                        employeePosition.CreationDate = DateTime.Now;
+                    }
+                }
+                
+                db.EmployeesPositions.RemoveRange(db.EmployeesPositions.Where(employeePosition => employeePosition.EmployeeId == employee.Id));
+                db.EmployeesPositions.AddRange(employee.EmployeesPositions);
+
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+
+                return true;
+            }
+            return false;
+        }
+
+        // GET: Employees/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Employee employee = db.Employees.Find(id);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
